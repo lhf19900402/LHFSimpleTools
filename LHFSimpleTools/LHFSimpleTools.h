f174df4d915b76c96f1d3b1867b4d3fa29bb8afb
李辉方 <1291912893@qq.com>
2017-01-12 16:33:06 +0800

    1. 添加内容

--- LHFSimpleTools/LHFSimpleTools.h
+++ LHFSimpleTools/LHFSimpleTools.h
@@ -1,13 +1,64 @@
-//
-//  LHFSimpleTools.h
-//  test
-//
-//  Created by 李辉方 on 2017/1/12.
-//  Copyright © 2017年 topfine. All rights reserved.
-//
+/*================================= 一套自己的小工具 ================================*/
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface LHFSimpleTools : NSObject
+
+                     #pragma mark - 初始化
+/*================================= 初始化 ================================*/
+/**
+ 单例此工具
+ */
++(instancetype)sharedLHFSimpleTools;
+
+
+                     #pragma mark - UIImage 相关的工具 -
+/*================================= UIImage 相关的工具 ================================*/
+
+#pragma mark 通过图片上下文来压缩图片
+/**
+ 通过图片上下文来压缩图片
+ @param originalImage 原始图片
+ @param width 指定宽度
+ @return 返回压缩后的图片
+ */
++(UIImage *)getImageFromCurrentImageContext:(UIImage *)originalImage withWidth:(CGFloat)width;
+
+#pragma mark 图片倒任意大小圆角
+/**
+ 图片倒任意大小圆角
+ @param originalImage 原始图片
+ @param cornerRadius 圆角大小
+ @return 返回倒好角的图片
+ */
++(UIImage *)clipImage:(UIImage *)originalImage cornerRadius:(CGFloat)cornerRadius;
+
+#pragma mark 裁剪圆/椭圆图片
+/**
+ 裁剪圆/椭圆图片
+ @param originalImage 原始图片
+ @return 返回圆/椭圆图片
+ */
++(UIImage *)clipRoundImage:(UIImage *)originalImage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
--- LHFSimpleTools/LHFSimpleTools.m
+++ LHFSimpleTools/LHFSimpleTools.m
@@ -1,13 +1,66 @@
-//
-//  LHFSimpleTools.m
-//  test
-//
-//  Created by 李辉方 on 2017/1/12.
-//  Copyright © 2017年 topfine. All rights reserved.
-//
-
 #import "LHFSimpleTools.h"
 
 @implementation LHFSimpleTools
+
+
++(instancetype)sharedLHFSimpleTools{
+    
+    static LHFSimpleTools *instance;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[self alloc] init];
+    });
+    
+    return instance;
+}
+
+
++(UIImage *)getImageFromCurrentImageContext:(UIImage *)originalImage withWidth:(CGFloat)width {
+    CGFloat height = (width*originalImage.size.height)/originalImage.size.width;
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(width, height), NO, 0.0);
+    [originalImage drawInRect:CGRectMake(0, 0, width, height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+
++(UIImage *)clipImage:(UIImage *)originalImage cornerRadius:(CGFloat)cornerRadius {
+    // 01 开启图片上下文 第二个参数 NO 代表透明
+    UIGraphicsBeginImageContextWithOptions(originalImage.size, NO, 0.0);
+    // 02 添加一个圆角
+    CGRect rect = CGRectMake(0, 0, originalImage.size.width, originalImage.size.height);
+    [[UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:cornerRadius] addClip];
+    // 03 将图片画上去
+    [originalImage drawInRect:rect];
+    // 04 获取图片
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    // 05 关闭上下文
+    UIGraphicsEndImageContext();
+    // 06 返回图片
+    return image;
+}
+
+
++(UIImage *)clipRoundImage:(UIImage *)originalImage {
+    // 01 开启图片上下文 第二个参数 NO 代表透明
+    UIGraphicsBeginImageContextWithOptions(originalImage.size, NO, 0.0);
+    // 02 获得上下文
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    // 03 添加一个圆
+    CGRect rect = CGRectMake(0, 0, originalImage.size.width, originalImage.size.height);
+    CGContextAddEllipseInRect(context, rect);
+    // 04 剪切
+    CGContextClip(context);
+    // 05 将图片画上去
+    [originalImage drawInRect:rect];
+    // 06 获取图片
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    // 07 关闭上下文
+    UIGraphicsEndImageContext();
+    // 08 返回图片
+    return image;
+}
 
 @end
